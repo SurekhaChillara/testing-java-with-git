873b507a6fbdcb17b72847a6d464bec0a73a9f5e
Surekha <chillarasurekha11@gmail.com>
2020-10-23 17:28:19 -0400

    added Java program

---
+++ Hey.java
@@ -0,0 +1,8 @@
+public class Hey{
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.out.println("Hello from Java Program ..." +i);
+        }
+    }
+	
+}
